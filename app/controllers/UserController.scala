fe05d8f67ed3fb7fcfbfd843eb9f09e81674adda
sasakirione <sasakirione@gmail.com>
2022-04-20 21:43:07 +0900

    Add: add around user of Controller

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api.mvc.{AbstractController, Action, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import usecase.UserUseCase
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class UserController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-    /*def findUser(): Action[String] = Action.async(
-      OK()
-    )*/
+class UserController @Inject()(cc: ControllerComponents, uc: UserUseCase) extends AbstractController(cc) {
+    def findUser(userToken: String): Action[AnyContent] = Action.async {
+      uc.login(userToken).map(
+        Ok(_)
+      ).recover {
+        case exception: Exception => InternalServerError(exception.getMessage)
+      }
+    }
 }
